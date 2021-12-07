2443f85a438a2b1857ec05f8a27de8c0830179c0
student <student@ip-172-31-63-173.us-west-2.compute.internal>
2021-12-06 16:57:18 -0800

    sapce sip

---
+++ src/LeagueInvadersRunner.java
@@ -0,0 +1,22 @@
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+
+public class LeagueInvadersRunner {
+public static void main(String [] agrs) {
+
+JFrame frame;
+
+}
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+
+
+
+LeagueInvadersRunner(){
+	
+JFrame frame = new JFrame();
+}
+
+
+}
