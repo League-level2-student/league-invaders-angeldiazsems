34190bb17d556c0f5c9a622ccea7e1c921c6bd4e
student <student@ip-172-31-50-226.us-west-2.compute.internal>
2021-12-20 16:57:34 -0800

    eee

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,30 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+
+	Timer frameDraw;
+
+	Font titleFont = new Font("Arial", Font.PLAIN, 48);
+	Font startFont = new Font("Arial", Font.PLAIN, 20);
+	Font instructionFont = new Font("Arial", Font.PLAIN, 20);
+
+	GamePanel() {
+
+		Timer frameDraw = new Timer(1000 / 60, this);
+
+		frameDraw = new Timer(1000 / 60, this);
+		frameDraw.start();
+
+	}
 
 	@Override
 	public void paintComponent(Graphics g) {
@@ -33,35 +53,97 @@ public class GamePanel extends JPanel {
 	}
 
 	void drawMenuState(Graphics g) {
-g.setColor(Color.BLUE);
-g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("LEAGUE INVADERS", 20, 100);
+
+		g.setFont(startFont);
+		g.drawString("Press enter to start", 150, 250);
+
+		g.setFont(instructionFont);
+		g.drawString("Press space for instructions", 100, 400);
+
 	}
 
 	void drawGameState(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-			}
-	
+	}
 
 	void drawEndState(Graphics g) {
 		g.setColor(Color.RED);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.BLACK);
+		g.drawString("GAME OVER", 20, 100);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+		if (currentState == MENU) {
+			updateMenuState();
+		}
+		if (currentState == GAME) {
+			updateGameState();
+		}
+		if (currentState == END) {
+			updateEndState();
+		}
+		System.out.println("action");
+		// need to add panel re paint
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (currentState == END) {
+				currentState = MENU;
+			} else {
+				currentState++;
 			}
-	
+		}
 
-GamePanel(String Arial, Font.PLAIN, 48){
-	titleFont = new Font(Arial, Font.PLAIN, 48);
+		
+		
+		if (arg0.getKeyCode()==KeyEvent.VK_UP) {
+		    System.out.println("UP");
+		}
+		if(arg0.getKeyCode()==KeyEvent.VK_DOWN) {
+			System.out.println("DOWN");
+		}
+		if(arg0.getKeyCode()==KeyEvent.VK_LEFT) {
+			System.out.println("LEFT");
+		}
+		if(arg0.getKeyCode()==KeyEvent.VK_RIGHT) {
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-}
-
-
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,33 +1,30 @@
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
-	
-public static void main(String [] agrs) {
-	
-	LeagueInvaders invader = new LeagueInvaders();
-	invader.setup();
-}
-JFrame frame = new JFrame();
-GamePanel game = new GamePanel();
 
+	public static void main(String[] agrs) {
 
-LeagueInvaders(){
+		LeagueInvaders invader = new LeagueInvaders();
+		invader.setup();
+	}
+
 	JFrame frame = new JFrame();
-	GamePanel game = new GamePanel();
-}
+	GamePanel panel = new GamePanel();
 
+	LeagueInvaders() {
+		JFrame frame = new JFrame();
+		GamePanel panel = new GamePanel();
+	}
 
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
-	
-	
-void setup() {
-	frame.add(game);
-	frame.setVisible(true);
-	frame.setSize(WIDTH,HEIGHT);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-}
 
+	void setup() {
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(panel);
+	}
 
 }
